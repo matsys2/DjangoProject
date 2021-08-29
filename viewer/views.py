74f0e590dac6c89ec7af95731363c08227e3d0e5
Riccardo <adam.wabicki@gmail.com>
2021-08-29 14:14:36 +0100

    Adding movie edit form

--- viewer/views.py
+++ viewer/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, FormView
+from django.views.generic import ListView, CreateView, UpdateView
 
 from viewer.models import Movie
 from viewer.forms import MovieForm
@@ -15,7 +15,7 @@ class MoviesView(ListView):
     model = Movie
 
 
-class MovieCreateView(FormView):
+class MovieCreateView(CreateView):
     template_name = 'form.html'
     form_class = MovieForm
     # adres pobrany z URLs na ktory zostaniemy przekierowani
@@ -23,25 +23,23 @@ class MovieCreateView(FormView):
     success_url = reverse_lazy('movie_create')
 
     # co ma sie dziac gdy formularz przeszedl walidacje
-    def form_valid(self, form):
-        # wywolanie metody form_valid z klasy nadrzednej (FormView)
-        # bedziemy zwracac wynik z niej uzyskany
-        result = super().form_valid(form)
-        # w obiekcie cleaned_data przechowujemy wynik dzialania
-        # funkcji "czyszczacych"
-        cleaned_data = form.cleaned_data
-        # zapisujemy do bazy nowy film:
-        Movie.objects.create(
-            title=cleaned_data['title'],
-            genre=cleaned_data['genre'],
-            rating=cleaned_data['rating'],
-            released=cleaned_data['released'],
-            description=cleaned_data['description'],
-        )
-        return result
-        # Zwracamy result - patrz komentarz nad form_valid
 
     # co ma sie dziac gdy formularz nie przejdzie walidacji
+    def form_invalid(self, form):
+        # odkladamy w logach informacje o opreacji
+        LOGGER.warning('User provided invalid data')
+        # zwracamy wynik dzialania pierwotnej funkcji form_ib=nvalid
+        return super().form_invalid(form)
+
+
+class MovieUpdateView(UpdateView):
+    template_name = 'form.html'
+    form_class = MovieForm
+    # adres pobrany z URLs na ktory zostaniemy przekierowani
+    # gdy walidacja sie powiedzie(movie_create pochodzi  z name!!)
+    success_url = reverse_lazy('index')
+    model = Movie
+
     def form_invalid(self, form):
         # odkladamy w logach informacje o opreacji
         LOGGER.warning('User provided invalid data')
